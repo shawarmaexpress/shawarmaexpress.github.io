39be3afcfd3f5b6f33505332c71fe95b70df7031
Iman Ghader <iman.ghader@shopify.com>
2016-12-05 00:17:49 -0500

    added new hours bar

---
+++ js/hours-old.js
@@ -0,0 +1,145 @@
+var times = [ 
+    { open: 11, close: 1 }, //Sunday
+    { open: 10.5, close: 22 }, //Monday
+    { open: 10.5, close: 22 }, //Tuesday
+    { open: 10.5, close: 25 }, //Wednesday
+    { open: 10.5, close: 1 }, //Thursday
+    { open: 10.5, close: 3.75 }, //Friday
+    { open: 11, close: 3.75 }, //Saturday { open1: 11.5, close1: 14, open2: 17, close2: 22 }
+];
+var date = new Date();
+date.setDate(23);
+var openingTimes = openingHours(date);
+var openClosed = false; // closed by default
+var hour = date.getHours() 
+var message = 'Our hours today are '; // message if open
+
+
+
+// check that there are opening times for today
+if (hasSimpleOpeningHours(openingTimes)){
+    openClosed = openingTimes.open <= hour && hour < openingTimes.close;
+} 
+// else if (hasComplexOpeningHours(openingTimes))  {
+//     openClosed = (openingTimes.open1 <= hour && hour < openingTimes.close1) 
+//     || (openingTimes.open2 <= hour && hour < openingTimes.close2);
+// }
+
+if (openClosed) {
+    if (hasSimpleOpeningHours(openingTimes)){
+        message += formatHours(openingTimes.open) + ' to ' + formatHours(openingTimes.close);
+    } //  else if (hasComplexOpeningHours(openingTimes)) {
+    //     if (openingTimes.open1 <= hour && hour < openingTimes.close1) {
+    //         message = 'Lunch hours today are ' + formatHours(openingTimes.open1) + ' to ' + formatHours(openingTimes.close1);
+    //     } else if (openingTimes.open2 <= hour && hour < openingTimes.close2) {
+    //         message = 'Dinner hours today are ' + formatHours(openingTimes.open2) + ' to ' + formatHours(openingTimes.close2);
+    //     }
+    // }
+}
+
+if (!openClosed){
+    if (hasSimpleOpeningHours(openingTimes)){
+        //Work out when we next open...
+        if (hour < openingTimes.open){ // open later on today.
+            var openAt = new Date();
+            message = 'We will be opening at  ' + formatHours(openingTimes.open) + ' today.';
+        }
+        else {
+            var foundNextOpenDay = false;
+            var nextOpenDay;
+            for (var i = 1; !foundNextOpenDay && i < 7; i++){
+                nextOpenDay = new Date(date.setDate(date.getDate() + 1)); // Add a day
+                openingTimes = openingHours(nextOpenDay);
+                if (hasSimpleOpeningHours(openingTimes) || hasComplexOpeningHours(openingTimes)){
+                    foundNextOpenDay = true; // exit the for loop
+                    if (hasSimpleOpeningHours(openingTimes)) {
+                        message = 'We will open ' + (i > 1 ? formatDay(nextOpenDay) : 'tomorrow') + 
+                              ' at ' + formatHours(openingTimes.open) + '.';
+                    } 
+                    // else if (hasComplexOpeningHours(openingTimes)) {
+                    //     message = 'We will open ' + (i > 1 ? formatDay(nextOpenDay) : 'tomorrow') + 
+                    //           ' at ' + formatHours(openingTimes.open1) + '.';
+                    // }
+                }
+            }
+            if (!foundNextOpenDay){
+                // No longer in business!
+                message = 'Sorry, we are closed for business.';
+            }
+        }
+    } //   else if (hasComplexOpeningHours(openingTimes)) {
+    //     //Work out when we next open...
+    //     if (hour < openingTimes.open1){ // open later on today.
+    //         var openAt = new Date();
+    //         message = 'We will be opening at  ' + formatHours(openingTimes.open1) + ' today.';
+    //     } else if (hour < openingTimes.open2) {
+    //         var openAt = new Date();
+    //         message = 'We will be opening at  ' + formatHours(openingTimes.open2) + ' today.';
+    //     }
+        else {
+            var foundNextOpenDay = false;
+            var nextOpenDay;
+            for (var i = 1; !foundNextOpenDay && i < 7; i++){
+                nextOpenDay = new Date(date.setDate(date.getDate() + 1)); // Add a day
+                openingTimes = openingHours(nextOpenDay);
+                if (hasSimpleOpeningHours(openingTimes)) { // hasComplexOpeningHours(openingTimes)){
+                    foundNextOpenDay = true; // exit the for loop
+                    if (hasSimpleOpeningHours(openingTimes)) {
+                        message = 'We will open ' + (i > 1 ? formatDay(nextOpenDay) : 'tomorrow') + 
+                              ' at ' + formatHours(openingTimes.open) + '.';
+                    } 
+                    // else if (hasComplexOpeningHours(openingTimes)) {
+                    //     message = 'We will open ' + (i > 1 ? formatDay(nextOpenDay) : 'tomorrow') + 
+                    //           ' at ' + formatHours(openingTimes.open1) + '.';
+                    // }
+                }
+            }
+            if (!foundNextOpenDay){
+                // No longer in business!
+                message = 'Sorry, we are closed for business.';
+            
+        }
+    }
+}
+
+function hasSimpleOpeningHours(openingTimes){
+    return openingTimes.hasOwnProperty('open') && openingTimes.hasOwnProperty('close');
+}
+
+// function hasComplexOpeningHours(openingTimes){
+//     return openingTimes.hasOwnProperty('open1') && openingTimes.hasOwnProperty('close1') && 
+//     openingTimes.hasOwnProperty('open2') && openingTimes.hasOwnProperty('close2');
+// }
+
+function formatHours(hour){
+    var amPm = hour > 11 ? 'pm' : 'am';
+    var time;
+    if (hour === 0) { // If, for whatever reason, you open at midnight!
+        time = 12;
+    }
+    else {
+        time = hour > 12 ? hour - 12 : hour;
+    }
+
+    return time + amPm;
+}
+
+function formatDay(date){
+    var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
+                    'Thursday', 'Friday', 'Saturday'];
+    return dayNames[date.getDay()];
+}
+
+function openingHours(date){
+    var dayOfWeek = date.getDay(); // 0 is Sunday, 1 is Monday, etc...
+    var openingTimes = times[dayOfWeek];
+
+    return openingTimes;
+}
+
+$(document).ready(function(){
+    document.getElementById('hours').innerHTML = message;        
+});
+
+
+
--- js/hours.js
+++ js/hours.js
@@ -1,138 +1,18 @@
-var times = [ 
-    { open: 17, close: 22 }, //Sunday
-    { open: 10.5, close: 22 }, //Monday
-    { open: 10.5, close: 22 }, //Tuesday
-    { open: 10.5, close: 22 }, //Wednesday
-    { open: 10.5, close: 22 }, //Thursday
-    { open: 10.5, close: 22 }, //Friday
-    { open1: 11.5, close1: 14, open2: 17, close2: 22 }, //Saturday
-];
-var date = new Date();
-var openingTimes = openingHours(date);
-var openClosed = false; // closed by default
-var hour = date.getHours() 
-var message = 'Our hours today are '; // message if open
-
-
-
-// check that there are opening times for today
-if (hasSimpleOpeningHours(openingTimes)){
-    openClosed = openingTimes.open <= hour && hour < openingTimes.close;
-} else if (hasComplexOpeningHours(openingTimes))  {
-    openClosed = (openingTimes.open1 <= hour && hour < openingTimes.close1) 
-    || (openingTimes.open2 <= hour && hour < openingTimes.close2);
-}
-
-if (openClosed) {
-    if (hasSimpleOpeningHours(openingTimes)){
-        message += formatHours(openingTimes.open) + ' to ' + formatHours(openingTimes.close);
-    } else if (hasComplexOpeningHours(openingTimes)) {
-        if (openingTimes.open1 <= hour && hour < openingTimes.close1) {
-            message = 'Lunch hours today are ' + formatHours(openingTimes.open1) + ' to ' + formatHours(openingTimes.close1);
-        } else if (openingTimes.open2 <= hour && hour < openingTimes.close2) {
-            message = 'Dinner hours today are ' + formatHours(openingTimes.open2) + ' to ' + formatHours(openingTimes.close2);
-        }
-    }
-}
-
-if (!openClosed){
-    if (hasSimpleOpeningHours(openingTimes)){
-        //Work out when we next open...
-        if (hour < openingTimes.open){ // open later on today.
-            var openAt = new Date();
-            message = 'We will be opening at  ' + formatHours(openingTimes.open) + ' today.';
-        }
-        else {
-            var foundNextOpenDay = false;
-            var nextOpenDay;
-            for (var i = 1; !foundNextOpenDay && i < 7; i++){
-                nextOpenDay = new Date(date.setDate(date.getDate() + 1)); // Add a day
-                openingTimes = openingHours(nextOpenDay);
-                if (hasSimpleOpeningHours(openingTimes) || hasComplexOpeningHours(openingTimes)){
-                    foundNextOpenDay = true; // exit the for loop
-                    if (hasSimpleOpeningHours(openingTimes)) {
-                        message = 'We will open ' + (i > 1 ? formatDay(nextOpenDay) : 'tomorrow') + 
-                              ' at ' + formatHours(openingTimes.open) + '.';
-                    } else if (hasComplexOpeningHours(openingTimes)) {
-                        message = 'We will open ' + (i > 1 ? formatDay(nextOpenDay) : 'tomorrow') + 
-                              ' at ' + formatHours(openingTimes.open1) + '.';
-                    }
-                }
-            }
-            if (!foundNextOpenDay){
-                // No longer in business!
-                message = 'Sorry, we are closed for business.';
-            }
-        }
-    }  else if (hasComplexOpeningHours(openingTimes)) {
-        //Work out when we next open...
-        if (hour < openingTimes.open1){ // open later on today.
-            var openAt = new Date();
-            message = 'We will be opening at  ' + formatHours(openingTimes.open1) + ' today.';
-        } else if (hour < openingTimes.open2) {
-            var openAt = new Date();
-            message = 'We will be opening at  ' + formatHours(openingTimes.open2) + ' today.';
-        }
-        else {
-            var foundNextOpenDay = false;
-            var nextOpenDay;
-            for (var i = 1; !foundNextOpenDay && i < 7; i++){
-                nextOpenDay = new Date(date.setDate(date.getDate() + 1)); // Add a day
-                openingTimes = openingHours(nextOpenDay);
-                if (hasSimpleOpeningHours(openingTimes) || hasComplexOpeningHours(openingTimes)){
-                    foundNextOpenDay = true; // exit the for loop
-                    if (hasSimpleOpeningHours(openingTimes)) {
-                        message = 'We will open ' + (i > 1 ? formatDay(nextOpenDay) : 'tomorrow') + 
-                              ' at ' + formatHours(openingTimes.open) + '.';
-                    } else if (hasComplexOpeningHours(openingTimes)) {
-                        message = 'We will open ' + (i > 1 ? formatDay(nextOpenDay) : 'tomorrow') + 
-                              ' at ' + formatHours(openingTimes.open1) + '.';
-                    }
-                }
-            }
-            if (!foundNextOpenDay){
-                // No longer in business!
-                message = 'Sorry, we are closed for business.';
-            }
-        }
-    }
-}
-
-function hasSimpleOpeningHours(openingTimes){
-    return openingTimes.hasOwnProperty('open') && openingTimes.hasOwnProperty('close');
-}
-
-function hasComplexOpeningHours(openingTimes){
-    return openingTimes.hasOwnProperty('open1') && openingTimes.hasOwnProperty('close1') && 
-    openingTimes.hasOwnProperty('open2') && openingTimes.hasOwnProperty('close2');
-}
-
-function formatHours(hour){
-    var amPm = hour > 11 ? 'pm' : 'am';
-    var time;
-    if (hour === 0) { // If, for whatever reason, you open at midnight!
-        time = 12;
-    }
-    else {
-        time = hour > 12 ? hour - 12 : hour;
-    }
-
-    return time + amPm;
-}
-
-function formatDay(date){
-    var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
-                    'Thursday', 'Friday', 'Saturday'];
-    return dayNames[date.getDay()];
-}
-
-function openingHours(date){
-    var dayOfWeek = date.getDay(); // 0 is Sunday, 1 is Monday, etc...
-    var openingTimes = times[dayOfWeek];
-
-    return openingTimes;
+function getStoreHours() {
+    var storeHourMessages = new Array(7);
+    storeHourMessages[0] = "Our Sunday hours are 11am to 1am"
+    storeHourMessages[1] = "Our Monday hours are 10:30am to 11pm"
+    storeHourMessages[2] = "Our Tuesday hours are 10:30am to 11pm"
+    storeHourMessages[3] = "Our Wednesday hours are 10:30am to 1am"
+    storeHourMessages[4] = "Our Thursday hours are 10:30am to 1am"
+    storeHourMessages[5] = "Our Friday hours are 10:30am to 1am"
+    storeHourMessages[6] = "Our Saturday hours are 11am to 1am"
+    
+    var currentDate = new Date();
+    var message = storeHourMessages[currentDate.getDay()];
+    document.getElementById('fixed-hours').innerHTML = message;        
 }
 
 $(document).ready(function(){
-    document.getElementById('hours').innerHTML = message;        
+    getStoreHours();
 });
