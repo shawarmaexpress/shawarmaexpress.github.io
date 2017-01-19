6bfa6638a384a4160c37d5d456a7a52c2a3fbbb3
Iman Ghader <iman.ghader@shopify.com>
2017-01-18 23:25:05 -0500

    Jan Update Debug v3

--- js/hours.js
+++ js/hours.js
@@ -1,12 +1,12 @@
 function getStoreHours() {
     var storeHourMessages = new Array(7);
-    storeHourMessages[0] = "Our Sunday hours are 11am to 1am"
-    storeHourMessages[1] = "Our Monday hours are 10:30am to 11pm"
-    storeHourMessages[2] = "Our Tuesday hours are 10:30am to 11pm"
-    storeHourMessages[3] = "Our Wednesday hours are 10:30am to 1am"
-    storeHourMessages[4] = "Our Thursday hours are 10:30am to 1am"
-    storeHourMessages[5] = "Our Friday hours are 10:30am to 1am"
-    storeHourMessages[6] = "Our Saturday hours are 11am to 1am"
+    storeHourMessages[0] = "Our Sunday hours are 10:30am to 12am"
+    storeHourMessages[1] = "Our Monday hours are 10:30am to 10pm"
+    storeHourMessages[2] = "Our Tuesday hours are 10:30am to 10pm"
+    storeHourMessages[3] = "Our Wednesday hours are 10:30am to 10pm"
+    storeHourMessages[4] = "Our Thursday hours are 10:30am to 12am"
+    storeHourMessages[5] = "Our Friday hours are 10:30am to 12am"
+    storeHourMessages[6] = "Our Saturday hours are 10:30am to 12am"
     
     var currentDate = new Date();
     var message = storeHourMessages[currentDate.getDay()];
