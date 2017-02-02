65e2174305ebf11eda7701b0c824f486357cd133
Iman Ghader <iman.ghader@shopify.com>
2017-02-01 22:11:21 -0500

    Changed hours, added UberEats desktop menu

--- js/hours.js
+++ js/hours.js
@@ -1,12 +1,12 @@
 function getStoreHours() {
     var storeHourMessages = new Array(7);
-    storeHourMessages[0] = "Our Sunday hours are 11am to 12am"
+    storeHourMessages[0] = "Our Sunday hours are 11am to 10pm"
     storeHourMessages[1] = "Our Monday hours are 10:30am to 10pm"
     storeHourMessages[2] = "Our Tuesday hours are 10:30am to 10pm"
     storeHourMessages[3] = "Our Wednesday hours are 10:30am to 10pm"
-    storeHourMessages[4] = "Our Thursday hours are 10:30am to 12am"
-    storeHourMessages[5] = "Our Friday hours are 10:30am to 12am"
-    storeHourMessages[6] = "Our Saturday hours are 11am to 12am"
+    storeHourMessages[4] = "Our Thursday hours are 10:30am to 10pm"
+    storeHourMessages[5] = "Our Friday hours are 10:30am to 10pm"
+    storeHourMessages[6] = "Our Saturday hours are 11am to 10pm"
     
     var currentDate = new Date();
     var message = storeHourMessages[currentDate.getDay()];
